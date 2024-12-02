ebe6a494e1fd024d51113defeda08b1ea58c5f12
Linda Postniece <linda.postniece@gmail.com>
2024-12-02 21:41:01 +1100

    Day 2 part 2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,57 @@
 ﻿using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
 
-Console.WriteLine("Advent Day 1");
+/*Console.WriteLine("Advent Day 1");
 
-var pair = ParseText(args[0]);
+var pair = ParseDay1Text(args[0]);
 Console.WriteLine(SumOfPairs(pair.Item1, pair.Item2));
-Console.WriteLine(SimilarityScore(pair.Item1, pair.Item2));
+Console.WriteLine(SimilarityScore(pair.Item1, pair.Item2));*/
+
+Console.WriteLine("Advent Day 2");
+var levels = ParseDay2Text(args[0]);
+Console.WriteLine(levels.Where(IsLevelSafe).Count());
+
+static bool IsGapSafe(int a, int b)
+{
+    var diff = Math.Abs(a - b);
+    return diff >= 1 && diff <= 3;
+}
+
+static bool IsLevelSafe(IEnumerable<int> level)
+{
+    // special case - 0 or 1 always safe
+    if (level.Count() <= 1)
+    {
+        return true;
+    }
+
+    // general case, first gap sets the pattern for ascending or descending
+
+    bool ascending = level.ElementAt(0) < level.ElementAt(1);
+
+    bool allSafe = true;
+    for (int i = 0; i < level.Count() - 1; i++)
+    {
+        allSafe &= IsGapSafe(level.ElementAt(i), level.ElementAt(i+1)) 
+                && (ascending ? 
+                              (level.ElementAt(i) < level.ElementAt(i+1)) 
+                              : level.ElementAt(i) > level.ElementAt(i + 1));
+    }
+
+    return allSafe;
+}
+
+static IEnumerable<int> LineToArray(String line)
+{
+    string[] arr = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    return arr.Select(Int32.Parse);
+}
+
+static IEnumerable<IEnumerable <int>> ParseDay2Text(String fileName)
+{
+    return File.ReadLines(fileName).Select(LineToArray);
+}
 
 static int SimilarityScore(List<int> left, List<int> right)
 {
@@ -31,7 +78,7 @@ static int SumOfPairs(List<int> left, List<int> right)
                                         : s + (pair.Item1 - pair.Item2));
 }
 
-static (List<int>, List<int>) ParseText(String fileName)
+static (List<int>, List<int>) ParseDay1Text(String fileName)
 {
     var left = new List<int>();
     var right = new List<int>();
