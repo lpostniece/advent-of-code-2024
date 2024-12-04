f2dedf0bad9938e1f925d3885dbc8ef3e3fbadf0
Linda Postniece <linda.postniece@gmail.com>
2024-12-04 19:27:30 +1100

    Day 4 part 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Data;
+using System.Text.RegularExpressions;
 
 /*Console.WriteLine("Advent Day 1");
 
@@ -12,7 +13,7 @@ var levels = ParseDay2Text(args[0]);
 Console.WriteLine("Normal safety: " + levels.Where(IsLevelSafe).Count());
 Console.WriteLine("Safety with dampening: " + levels.Where(IsLevelSafeWithDampening).Count());*/
 
-Console.WriteLine("Advent Day 3");
+/*Console.WriteLine("Advent Day 3");
 
 var pairs = ParseTextForMulDoDont(File.ReadAllText(args[0]));
 
@@ -20,7 +21,152 @@ var multiplied = pairs.Select(pair => pair.Item1 * pair.Item2);
 
 var summed = multiplied.Sum();
 
-Console.WriteLine(summed);
+Console.WriteLine(summed);*/
+
+Console.WriteLine("Advent Day 4");
+
+var array = ParseXmasGrid(args[0]);
+
+Console.WriteLine(array);
+
+// first dimension moves across
+// second dimension moves up
+
+Console.WriteLine(WordOccurences(array, "XMAS"));
+
+int WordOccurences(char[,] array, string word)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int count = CountOfWordStartsAtPosition(array, "XMAS", i,j);
+            sum += count;
+        }
+    }
+
+    return sum;
+}
+
+int CountOfWordStartsAtPosition(char[,] array, string word, int x, int y)
+{
+    var directionList = new List<DirectionEnum>()
+    {
+        DirectionEnum.Up,
+        DirectionEnum.Down,
+        DirectionEnum.Right,
+        DirectionEnum.Left,
+        DirectionEnum.UpRight,
+        DirectionEnum.DownRight,
+        DirectionEnum.UpLeft,
+        DirectionEnum.DownLeft
+    };
+
+    int count = 0;
+
+    foreach (var dir in directionList)
+    {
+        if (DoesWordStartAtPositionWithDirection(array, word, x, y, dir))
+        {
+            count++; 
+        }
+    }
+
+    return count;
+}
+
+bool DoesWordStartAtPositionWithDirection(char[,] array, string word, int x, int y, DirectionEnum dir)
+{
+    var letterMatch = array[x, y].Equals(word.First());
+
+    if (letterMatch)
+    {
+        if (word.Length == 1)
+        {
+            Console.WriteLine($"DoesWordStartAtPositionWithDirection {word} {x} {y} {dir}");
+            return true;
+        }
+        else
+        {
+            var (nextX, nextY, isValid) = GetNextPosition(dir, array, x, y);
+
+            if (isValid)
+            {
+                return DoesWordStartAtPositionWithDirection(array, word.Substring(1), nextX, nextY, dir);
+            }
+        }
+    }
+
+    return false;
+}
+
+
+(int, int, bool) GetNextPosition(DirectionEnum dir, char[,] array, int currentX, int currentY)
+{
+    int nextX = currentX;
+    int nextY = currentY;
+
+    switch(dir)
+    {
+        case DirectionEnum.Left:
+            nextX = nextX - 1;
+            break;
+        case DirectionEnum.Right:
+            nextX = nextX + 1;
+            break;
+        case DirectionEnum.Up:
+            nextY = nextY - 1;
+            break;
+        case DirectionEnum.Down:
+            nextY = nextY + 1;
+            break;
+        case DirectionEnum.UpRight:
+            nextY = nextY - 1;
+            nextX = nextX + 1;
+            break;
+        case DirectionEnum.DownRight:
+            nextY = nextY + 1;
+            nextX = nextX + 1;
+            break;
+        case DirectionEnum.DownLeft:
+            nextY = nextY + 1;
+            nextX = nextX - 1;
+            break;
+        case DirectionEnum.UpLeft:
+            nextY = nextY - 1;
+            nextX = nextX - 1;
+            break;
+    }
+
+    if (nextX >= 0 && nextX < array.GetLength(0) && nextY >= 0 && nextY < array.GetLength(1))
+        return (nextX, nextY, true);
+    else 
+        return (-1, -1, false);
+}
+
+static char[,] ParseXmasGrid(string inputFileName)
+{
+    IEnumerable<char[]> listOfArrays = File.ReadLines(inputFileName).Select(str => str.ToCharArray());
+    var lengthOfFirst = listOfArrays.FirstOrDefault().Length;
+    if (listOfArrays.Any(arr => arr.Length != lengthOfFirst))
+    {
+        throw new Exception("Non-rectangular array");
+    }
+
+    char[,] array = new char[listOfArrays.Count(), lengthOfFirst];
+
+    for (int down = 0; down < listOfArrays.Count(); down++)
+    {
+        for (int across = 0; across < lengthOfFirst; across++)
+            {
+                array[down, across] = listOfArrays.ElementAt(down).ElementAt(across);
+            }
+    }
+
+    return array;
+}
 
 static int ParseMatchGroup(Match match, int index)
 {
@@ -175,4 +321,16 @@ static (int, int) LineToPair(String line)
 {
     string[] pair = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
     return (Int32.Parse(pair[0].Trim()), Int32.Parse(pair[1].Trim()));
+}
+
+enum DirectionEnum
+{
+    Up,
+    Down,
+    Right,
+    Left,
+    UpRight,
+    DownRight,
+    UpLeft,
+    DownLeft
 }
